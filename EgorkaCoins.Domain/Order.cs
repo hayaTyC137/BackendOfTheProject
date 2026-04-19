97ca8b4f24cac021932e2a93d6a9a8e19124fdb3
hayaTyC137 <kigore17@mail.ru>
2026-04-19 15:42:05 +0300

    уйдя от реализации local storage для последующего результата внесения данных в базу данных, а также ее полная реализация

---
+++ EgorkaCoins.Domain/Order.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EgorkaCoins.Domain
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string GameName { get; set; } = string.Empty;
+        public string GameColor { get; set; } = string.Empty;
+        public string Item { get; set; } = string.Empty;
+        public string Amount { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Status { get; set; } = "pending";
+        public DateTime CreatedAt { get; set; }
+    }
+}
